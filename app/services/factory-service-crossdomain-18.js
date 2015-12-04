6d0fb5fb372a6144851676820e4615a33d9485ac
Udupa, Prashanth <prashanth.udupa@fmr.com>
2015-12-04 09:48:04 +0530

    Added a cross domain data example.

---
+++ app/services/factory-service-crossdomain-18.js
@@ -0,0 +1,12 @@
+/**
+ * Created by a487037 on 12/02/2015.
+ */
+(function () {
+    'use strict';
+    /*global angular*/
+    angular.module('app').factory('dataFactory', function ($http) {
+        return {
+            hd: $http.get('http://localhost:1234/heros')
+        };
+    })
+}());
